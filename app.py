985ca60725eea182e985822c97f0558c7f632ac7
dezy1ex <dezy1ex@gmail.com>
2019-07-19 01:07:04 +0400

    add live page mode

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about_me")
+def test():
+    return render_template("about.html")
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("not_found.html"), 404
+
+
 if __name__ == "__main__":
     app.run()
